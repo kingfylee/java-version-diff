cd11cb4ff0b6ea023699930dac205671f14a740e
kingfylee <572368814@qq.com>
2018-07-31 20:46:19 +0800

    java5 generics

---
+++ java5/src/main/java/com/kf/java5/Generics.java
@@ -0,0 +1,13 @@
+package com.kf.java5;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Generics {
+
+    /**
+     *
+     */
+    Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+
+}
